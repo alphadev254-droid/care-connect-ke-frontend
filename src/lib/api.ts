dc601fb213409e1c7ad92f22d14c1fdea00e38db
alphadev254-droid <alphadev254@gmail.com>
2026-04-03 09:53:16 +0300

    increased timout

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,7 +8,7 @@ export const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 30000,
+  timeout: 120000,
   withCredentials: true, // Send cookies with requests
 });
 
